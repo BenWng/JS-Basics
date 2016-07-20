a57b29f7dd8b1b1021adbd6afe15c22ffb5eea8d
BenWng <wnghan@gmail.com>
2016-07-20 03:00:49 -0700

    Ready to push to github

--- callback.js
+++ callback.js
@@ -1,3 +1,22 @@
+function fn2 (a,b, callback){
+    var c=a+b;
+    var d=a-b;
+    callback(d,c);
+}
+
+(function (){
+    fn2(3,4,function(res1,res2){
+        console.log(res1);
+        console.log(res2);
+    })
+})();
+
+
+///////////////
+
+
+
+
 function fn1 (a,b, callback){
     var c=a+b;
     var d=a-b;
@@ -11,3 +30,21 @@ function fn1 (a,b, callback){
     });
     console.log(x);
 })();
+
+
+
+///////////////
+function fn3(a,b, callback ){
+    var c=a+b;
+    var d=a-b;
+}
+
+
+(function(){
+    fn3(5,1, function(){
+        console.log("haha");
+    });
+})();
+
+
+
